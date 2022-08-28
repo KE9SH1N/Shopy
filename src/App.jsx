4280819e49f4ad4b2d004802f27e69689485dd29
unknown <k3nshi9@gmail.com>
2022-08-28 18:51:08 +0600

    fresh new ecommerce site with react - navbar and slider update

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import Home from "./pages/Home";
+const App = () => {
+  return (
+  <div> <Home /> </div>
+  )
+  
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Announcement from '../components/Announcement'
+import Navbar from "../components/Navbar"
+import Slider from '../components/Slider'
+const home = () => {
+  return (
+
+    <div>
+      <Announcement/>
+      <Navbar/>
+      <Slider/>
+      </div>
+
+  )
+}
+
+export default home
