ba79f7a41f039da6c76faf0a7ad7fd900906e53b
unknown <k3nshi9@gmail.com>
2022-09-05 18:14:14 +0600

    adding product color and size filter box

---
+++ src/components/FilterProduct.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    
+`
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const Filter = styled.div`
+    margin-top: 20px;
+    margin-bottom: 20px;
+`
+
+const Select = styled.select`
+    padding: 10px;
+    margin-right:20px;
+`
+
+const Option = styled.option`
+    
+`
+
+
+const FilterProduct = () => {
+  return (
+    <Container>
+          <FilterContainer>
+              <Filter>
+                  <Select>
+                      <Option disabled selected>
+                          Color
+                      </Option>
+                      <Option>
+                          White
+                      </Option>
+                      <Option>
+                          Black
+                      </Option>
+                      <Option>
+                          Red
+                      </Option>
+                      <Option>
+                          Green
+                      </Option>
+                      <Option>
+                          Blue
+                      </Option>
+                  </Select>
+
+
+                  <Select>
+                      <Option disabled selected>
+                          Size
+                      </Option>
+                      <Option>
+                          XS
+                      </Option>
+                      <Option>
+                          S
+                      </Option>
+                      <Option>
+                          M
+                      </Option>
+                      <Option>
+                          L
+                      </Option>
+                      <Option>
+                          XL
+                      </Option>
+                  </Select>
+              </Filter>
+          </FilterContainer>
+    </Container>
+  )
+}
+
+export default FilterProduct
